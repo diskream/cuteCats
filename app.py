34068772aedac4ea3c70af6a9f80a1b051e3897b
Diskream <diskream77@yandex.ru>
2022-04-01 17:48:10 +0300

    Добавлен полнотекстовый поиск

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect, send_file
 from flask_migrate import Migrate
 import base64
+from io import BytesIO, StringIO
+from PIL import Image
+from sqlalchemy import cast
+from sqlalchemy import func
 from pickle import dumps
 from models import db, CatsModel
 
@@ -16,6 +20,7 @@ migrate = Migrate(app, db)
 with app.app_context():
     db.create_all()
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -24,9 +29,30 @@ def allowed_file(filename):
 def hello_world():  # put application's code here
     return render_template('index.html')
 
-@app.route('/cats')
-def cats():  # put application's code here
-    return render_template('cats.html')
+@app.route('/cats/page_<int:page>', methods=['POST', 'GET'])
+def cats(page=1):
+    if request.method == "POST":
+        return redirect(f'/search?query={request.form["query"]}')
+    cats = CatsModel.query.paginate(page, 5, False)
+    return render_template('cats.html', cats=cats)
+
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    if request.method == "POST":
+        return redirect(f'/search?query={request.form["query"]}')
+    query = request.args.get('query').replace(' ', '|')
+    # find = ((CatsModel.breed.like(query)) | (CatsModel.name.like(query)) |
+    #         (CatsModel.description.like(query))| (CatsModel.age.cast(db.String).like(query)))
+    filtered_cats = CatsModel.query.filter(CatsModel.__ts_vector__.match(query, postgresql_regconfig='russian')).\
+        paginate(1, 5, False)
+    return render_template('cats.html', cats = filtered_cats)
+
+@app.route('/cats/cat_<int:id>')
+def get_cat(id):
+    print(CatsModel.query.filter_by())
+    cat = CatsModel.query.get(id)
+    return render_template('cat_description.html', cat=cat, img=resize_image(cat.img, (300, 250)))
 
 @app.route('/add', methods=['POST', 'GET'])
 def add_cat():
@@ -56,6 +82,12 @@ def add_cat():
     else:
         return render_template('add.html')
 
+def resize_image(img: bytes, size: tuple) -> str:
+    buffer = BytesIO(base64.b64decode(img))
+    image = Image.open(buffer).resize(size, Image.ANTIALIAS)
+    buffer = BytesIO()
+    image.save(buffer, 'png')
+    return base64.b64encode(buffer.getvalue()).decode('utf-8')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,6 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy.types import TypeDecorator
+from sqlalchemy import func
+from sqlalchemy.dialects.postgresql import TSVECTOR
 db = SQLAlchemy()
+
+class TSVector(TypeDecorator):
+    impl = TSVECTOR
+
+
+def create_tsvector(*args):
+    exp = args[0]
+    for e in args[1:]:
+        exp += ' ' + e
+    return func.to_tsvector('russian', exp)
 
 class CatsModel(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -10,5 +22,11 @@ class CatsModel(db.Model):
     description = db.Column(db.TEXT())
     age = db.Column(db.Integer())
 
+    __ts_vector__ = db.Column(TSVector(), db.Computed(
+        "to_tsvector('russian', breed || ' ' || name || ' ' || description || ' ' || age::text)",
+    persisted=True))
+
+    __table_args__ = (db.Index('ix_cats_model___ts_vector__', __ts_vector__, postgresql_using='gin'),)
+
     def __repr__(self):
-        return f'{self.name}: {self.breed}, {self.age} месяцев.'
+        return f'{self.name}: {self.breed}, {self.age} месяцев'
